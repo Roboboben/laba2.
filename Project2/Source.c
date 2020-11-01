3fdf727d19105f527ea58c8345b98a740d39bed2
Roboboben <73778095+Roboboben@users.noreply.github.com>
2020-11-01 14:37:09 +0300

    Update Source.c

--- Project2/Source.c
+++ Project2/Source.c
@@ -1,24 +1,24 @@
-#include <stdio.h>      // Ввести строку, вывести пословно на экран
+#include <stdio.h>      // Enter a string and display it word-by-word on the screen
 int main()
 {
 	char arr[1000];
 	printf("Input string: \n");
-	fgets(arr, sizeof(arr), stdin); //Ввод массива (Записывает строку до Enter)
+	fgets(arr, sizeof(arr), stdin); 
 	int len = 0; 
 	int i = 0;
-	//Подсчет кол-во символов
+	
 	while (arr[len] != '\0')
 		len++;
-	// Защита от дурака
-	while ((!(arr[len] >= 'A') && (arr[len] <= 'Z')) || (!(arr[len] >= 'a') && (arr[len] <= 'z'))) {// символы 
+	
+	while ((!(arr[len] >= 'A') && (arr[len] <= 'Z')) || (!(arr[len] >= 'a') && (arr[len] <= 'z'))) {
 		len--;
 	}
-	//
+	
 	while (i <= len) {
-		while ((!(arr[i] >= 'A') && (arr[i] <= 'Z')) || (!(arr[i] >= 'a') && (arr[i] <= 'z'))) { //символы
+		while ((!(arr[i] >= 'A') && (arr[i] <= 'Z')) || (!(arr[i] >= 'a') && (arr[i] <= 'z'))) {
 			i++;
 		}
-		while ((arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= 'a' && arr[i] <= 'z')) { // буква
+		while ((arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= 'a' && arr[i] <= 'z')) { 
 			printf("%c", arr[i]);
 			i++;
 		}
